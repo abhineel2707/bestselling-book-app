1f4817a24f6315987676b9d1a4d67c5d97536fd5
abhineel rai <abhineel2707@gmail.com>
2023-06-25 02:09:47 +0530

    Initial commit: Set up bestselling book app with essential components, global styles, and static book images.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+function Book() {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Image() {
+  return (
+    <img
+      src="./images/book-interesting-facts.jpg"
+      alt="Interesting Facts For Curious Minds"
+    />
+  );
+}
+
+const Title = () => {
+  return <h2>Interesting Facts For Curious Minds</h2>;
+};
+
+function Author() {
+  const inlineHeadingStyles = {
+    color: '#617d98',
+    fontSize: '0.75rem',
+    marginTop: '0.5rem',
+  };
+  return (
+    // <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}>
+    //   Jordan Moore
+    // </h4>
+
+    <h4 style={inlineHeadingStyles}>Jordan Moore</h4>
+  );
+}
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+root.render(<BookList />);
